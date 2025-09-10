3bdfcadf94833bec48124644478e23fd78ff3078
Pedro Omar <pocl9812@gmail.com>
2025-09-10 09:21:44 -0400

    Initial Commit

---
+++ La Chispa/La_ChispaApp.swift
@@ -0,0 +1,17 @@
+//
+//  La_ChispaApp.swift
+//  La Chispa
+//
+//  Created by Pedro Omar  on 9/10/25.
+//
+
+import SwiftUI
+
+@main
+struct La_ChispaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
